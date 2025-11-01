0e4413223c480ef9a1652dcf7220ab6dfbc9a968
AqilYogaPramono <aqilyogapra1@gmail.com>
2025-11-01 19:02:53 +0700

    fix: login multiple role

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+
 const Users = require('../models/User')
+const Admin = require('../models/Admin')
+
 const router = express.Router()
 
 router.post('/register', async (req, res) => {
@@ -20,7 +24,7 @@ router.post('/register', async (req, res) => {
             return res.status(400).json({ message: 'Confirm Password is required'})
         }
 
-        if (await Users.checkEmail(email)) {
+        if (await Users.checkEmail(data)) {
             return res.status(400).json({ message: 'Email is already in use.' })
         }
 
@@ -48,8 +52,8 @@ router.post('/register', async (req, res) => {
 
         res.status(201).json({ message: 'User registered successfully.' })
     } catch (err) {
-        console.log(err)
-        res.status(500).json({ message: err.message })
+        console.error(err)
+        res.status(500).json({ message: 'Internal Server Error' })
     }
 })
 
@@ -66,16 +70,59 @@ router.post('/login', async (req, res) => {
             return res.status(400).json({ message: 'Password is required.' })
         }
 
-        const user = await Users.login(data)
+        let user = null
+        let role = null
 
-        const payload = {id: user.id}
+        user = await Users.login(data)
+        if (user) {
+            role = 'User'
+        } else {
+            user = await Admin.login(data)
+            if (user) {
+                role = 'Admin'
+            }
+        }
+
+        if (!user) {
+            return res.status(401).json({ message: 'Email not found' })
+        }
+
+        if (!await bcrypt.compare(password, user.password)) {
+            return res.status(401).json({ message: 'Incorrect password' })
+        }
+
+        if (user.status != 'Active') {
+            return res.status(403).json({ message: 'Account is not active' })
+        }
+
+        const payload = {id: user.id, role}
 
         const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '4h' })
 
         res.status(200).json({ token })
     } catch (err) {
-        console.log(err)
-        res.status(500).json({ message: err.message })
+        console.error(err)
+        res.status(500).json({ message: 'Internal Server Error' })
+    }
+})
+
+router.post('/logout', (req, res) => {
+    try {
+        const token = req.headers.authorization?.split(' ')[1]
+
+        if (!token) {
+            return res.status(401).json({ message: 'No token provided.' })
+        }
+        
+        jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+            if (err) {
+            return res.status(401).json({ message: 'Invalid token or token has expired.' })
+            }
+            res.status(200).json({ message: 'Logout successful.' })
+        })
+    } catch (err) {
+        console.error(err)
+        res.status(500).json({ message: 'Internal server error' })
     }
 })
 
