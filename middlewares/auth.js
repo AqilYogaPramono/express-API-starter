5249110301e1431697a5138e2aa2212d3c6a9eb8
AqilYogaPramono <aqilyogapra1@gmail.com>
2025-11-01 18:55:35 +0700

    fix: cheking role in jwt

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -20,7 +20,7 @@ function authorize(allowedRoles = []) {
         if (!req.user) {
             return res.status(403).json({ message: 'User not authenticated' })
         }
-        if (!allowedRoles.includes(req.user.userType)) {
+        if (!allowedRoles.includes(req.user.role)) {
             return res.status(403).json({ message: 'Access denied: insufficient permissions' })
         }
         next()
